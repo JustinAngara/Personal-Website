06e15bd3f8c9c17cd6eacefcff6244512c4bceb2
Justin Angara <justin.angara@gmail.com>
2024-07-21 03:30:54 -0500

    changed css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Routes from "./Pages/Routes.jsx"
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        
         <Routes />
     </React.StrictMode>
 
