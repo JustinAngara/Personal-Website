5e43ea02546baaad6009a00b4407ed71fb8fe44c
Justin Angara <justin.angara@gmail.com>
2024-07-23 20:47:26 -0500

    Setting up projs

--- src/Pages/Staff/Staff.jsx
+++ src/Pages/Staff/Staff.jsx
@@ -7,10 +7,14 @@ const Staff = () => {
             <Inset>
                 <h1>Projects</h1>
                 <StaffList></StaffList>
+
                 <div className="proj">
+                    <h2>Chess MiniMax Algorithm <br/> (incorporated with real life moves and a table base)</h2>
                     <iframe width="75%" height="700" src="//jsfiddle.net/AngaraJustin/dxqk26nv/223/embedded/result/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
                 </div>
+
                 
+
             </Inset>
         </StaffStyled>
     );
